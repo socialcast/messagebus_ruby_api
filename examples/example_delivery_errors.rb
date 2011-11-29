5838ee9fe0e5a3338d99530351ede51e5ae3a01a
Messenger <messenger@TUCKER.local>
2011-11-29 14:50:20 -0800

    update example_delivery_errors to supply start and end date parameters

--- examples/example_delivery_errors.rb
+++ examples/example_delivery_errors.rb
@@ -10,8 +10,13 @@ api_key="YOUR_ACCOUNT_API_KEY_GOES_HERE"
 client= MessagebusApi::Messagebus.new(api_key)
 
 # retrieves message delivery errors
+# the delivery_errors method optionally accepts startDate and endDate parameters which define the
+# range of dates to supply delivery errors for.  if these parameters are not supplied, startDate
+# defaults to 7 days ago and endDate defaults to today.  Format date strings as YYYY-MM-DD.
 begin
-  status = client.delivery_errors
+  start_date_str="2011-01-01"
+  end_date_str="2011-01-02"
+  status = client.delivery_errors(start_date_str, end_date_str)
 
   if status[:statusCode] == 200
     puts "Method returned successfully."
